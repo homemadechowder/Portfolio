770d9439cd79a4571ad3e077ba41440f91b4d2cf
Andrew Lu <awclu@ucdavis.edu>
2019-11-19 18:18:27 -0800

    slide wheel

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,38 +1,66 @@
+
 $(document).ready(function(){
     var letter = $(".box");
-    
-    //Obtain Mouse Information
+    var letter2 = $(".box2");
+    var audio = new Audio("./assets/sounds/ding.wav");
+    letter.click ();
+    //Obtain Mouse Scroll Information up or down
     window.addEventListener("wheel", event => {
         const delta = Math.sign(event.deltaY);
+        var unlock = $(".box").attr("style");
+        var unlock2 = $(".box2").attr("style");
+        
+
         console.log(delta);
+        console.log(unlock);
+        console.log(unlock2);
+        if (delta == -1){
+            letter.animate({ top: "-=100px" }, "normal");
+            letter2.animate({ top: "+=100px" }, "normal");
+        }
+        else if (delta == 1){
+            letter.animate({ top: "+=100px" }, "normal");
+            letter2.animate({ top: "-=100px" }, "normal");
+        }
+
+        if (unlock == "top: 200px;" && unlock2 == "top: 320px;"){
+            console.log("unlocked");
+            letter.fadeOut("slow");
+            letter2.fadeOut("slow");
+            audio.play();
+            $("#id").hide();
+            
+        }
     });
 
     
-    $(document).keyup(function(e) {
-        switch (e.which) {
+    
+
+    // $(document).keyup(function(e) {
+    //     switch (e.which) {
   
-        // Move Buttons (Keyboard Down)
-        case 40:
-          letter.animate({ top: "+=200px" }, "normal");
-          break;
+    //     // Move Buttons (Keyboard Down)
+    //     case 40:
+    //       letter.animate({ top: "+=200px" }, "normal");
+    //       break;
   
-          // Move Buttons (Keyboard Right)
-        case 39:
-          letter.animate({ left: "+=200px" }, "normal");
-          break;
+    //       // Move Buttons (Keyboard Right)
+    //     case 39:
+    //       letter.animate({ left: "+=200px" }, "normal");
+    //       break;
   
-          // Move Buttons (Keyboard Up)
-        case 38:
-          letter.animate({ top: "-=200px" }, "normal");
-          break;
+    //       // Move Buttons (Keyboard Up)
+    //     case 38:
+    //       letter.animate({ top: "-=200px" }, "normal");
+    //       break;
   
-          // Move Buttons (Keyboard Left)
-        case 37:
-          letter.animate({ left: "-=200px" }, "normal");
-          break;
+    //       // Move Buttons (Keyboard Left)
+    //     case 37:
+    //       letter.animate({ left: "-=200px" }, "normal");
+    //       break;
   
-        default:
-          break;
-        }
-      });
+    //     default:
+    //       break;
+    //     }
+    //   });
 })
